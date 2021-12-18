92f7a17e5141d9cdd0b5076987c14bf0bde2e9e4
Achot BARSEGHYAN <achot.barseghyan@gmail.com>
2021-12-19 00:06:58 +0100

    feat: update

--- src/main/java/com/itech/showtimeAPI/ShowtimeApiApplication.java
+++ src/main/java/com/itech/showtimeAPI/ShowtimeApiApplication.java
@@ -1,16 +1,7 @@
 package com.itech.showtimeAPI;
 
-import com.itech.showtimeAPI.admin.Admin;
-import com.itech.showtimeAPI.repository.AdminRepository;
-import com.itech.showtimeAPI.repository.UserRepository;
-import com.itech.showtimeAPI.user.User;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @SpringBootApplication
 public class ShowtimeApiApplication {
@@ -18,6 +9,7 @@ public class ShowtimeApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShowtimeApiApplication.class, args);
 	}
+
 
 
 }
