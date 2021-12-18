a41db4892ce2d58c87a81dd2b11788ca7c29a4ae
Achot BARSEGHYAN <achot.barseghyan@gmail.com>
2021-12-18 18:34:46 +0100

    feat: imports removed

--- src/main/java/com/itech/showtimeAPI/ShowtimeApiApplication.java
+++ src/main/java/com/itech/showtimeAPI/ShowtimeApiApplication.java
@@ -1,14 +1,7 @@
 package com.itech.showtimeAPI;
 
-import com.itech.showtimeAPI.repository.UserRepository;
-import com.itech.showtimeAPI.user.User;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @SpringBootApplication
 public class ShowtimeApiApplication {
--- src/main/java/com/itech/showtimeAPI/user/User.java
+++ src/main/java/com/itech/showtimeAPI/user/User.java
@@ -1,14 +1,12 @@
 package com.itech.showtimeAPI.user;
 
 import lombok.Data;
-import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Encrypted;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
