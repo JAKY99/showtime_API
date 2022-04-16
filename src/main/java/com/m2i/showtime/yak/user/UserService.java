881d92d1e090de8bc3afbf49162e4866f7f12a25
Achot BARSEGHYAN <achot.barseghyan@gmail.com>
2022-04-16 21:18:46 +0200

    User post,put and delete mapping added

--- src/main/java/com/m2i/showtime/yak/user/UserService.java
+++ src/main/java/com/m2i/showtime/yak/user/UserService.java
@@ -3,7 +3,9 @@ package com.m2i.showtime.yak.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -29,5 +31,48 @@ public class UserService {
         }
 
         userRepository.save(user);
+    }
+
+    public void deleteUser(Long userId) {
+
+        if (!userRepository.existsById(userId)){
+            throw new IllegalStateException("User does not exists");
+        }
+        userRepository.deleteById(userId);
+    }
+
+    @Transactional
+    public void updateUser(Long userId,
+                           User modifiedUser) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalStateException(("user with id "+ userId + "does not exists")));
+
+        if (modifiedUser.getFirstName() != null &&
+                modifiedUser.getFirstName().length() > 0 &&
+                !Objects.equals(user.getFirstName(), modifiedUser.getFirstName())) {
+            user.setFirstName(modifiedUser.getFirstName());
+        }
+
+        if (modifiedUser.getLastName() != null &&
+                modifiedUser.getLastName().length() > 0 &&
+                !Objects.equals(user.getLastName(), modifiedUser.getLastName())) {
+            user.setLastName(modifiedUser.getLastName());
+        }
+
+        if (modifiedUser.getCountry() != null &&
+                modifiedUser.getCountry().length() > 0 &&
+                !Objects.equals(user.getCountry(), modifiedUser.getCountry())) {
+            user.setCountry(modifiedUser.getCountry());
+        }
+
+        if (modifiedUser.getEmail() != null &&
+                modifiedUser.getEmail().length() > 0 &&
+                !Objects.equals(user.getEmail(), modifiedUser.getEmail())) {
+            if (userRepository.findUserByEmail(modifiedUser.getEmail()).isPresent()){
+                throw new IllegalStateException("email taken");
+            }
+            user.setEmail(modifiedUser.getEmail());
+        }
     }
 }
