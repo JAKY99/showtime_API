06b78bebe02100c28e168c46ca3d01b5754c3560
Achot Barseghyan <57489459+achot-barseghyan@users.noreply.github.com>
2022-04-16 17:09:40 +0200

    Spring project converted mongo to postgresql

---
+++ src/main/java/com/m2i/showtime/yak/user/UserService.java
@@ -0,0 +1,33 @@
+package com.m2i.showtime.yak.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+    public void addNewUser(User user) {
+
+        Optional<User> userOptional = userRepository.findUserByEmail(user.getEmail());
+
+        if (userOptional.isPresent()){
+            throw new IllegalStateException("email taken");
+        }
+
+        userRepository.save(user);
+    }
+}
