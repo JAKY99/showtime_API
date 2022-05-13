8b8fdf0fa9fe4a132cb01fd75c02480e7cdca210
Achot BARSEGHYAN <achot.barseghyan@gmail.com>
2022-05-13 22:42:48 +0200

    moviePackage

--- src/main/java/com/m2i/showtime/yak/user/User.java
+++ src/main/java/com/m2i/showtime/yak/user/User.java
@@ -1,9 +1,14 @@
 package com.m2i.showtime.yak.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.m2i.showtime.yak.movie.Movie;
 import lombok.Data;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "_user")
@@ -23,13 +28,30 @@ public class User {
     private Long id;
     private String firstName;
     private String lastName;
+    @Transient
+    private String fullName;
     private String email;
+    @Column(name = "profile_picture", length = 64)
+    private String profilePicture;
 
     @Column(name = "_password")
     private String password;
 
-    private LocalDate created;
     private String country;
+    private LocalDate dateCreated;
+    private Boolean deleted = false;
+    private Long nbWatchedMovies;
+    private Long nbWatchedTvShows;
+    private Float totalMovieWatchedTime;
+    private Float totalTvShowsWatchedTime;
+
+    @ManyToMany
+    @JoinTable(
+            name = "users_watched_movie",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "movie_id",referencedColumnName = "id")
+    )
+    private Collection<Movie> watchedMovies;
 
     public User() {
     }
@@ -39,27 +61,33 @@ public class User {
                 String lastName,
                 String email,
                 String password,
-                String country) {
+                String country,
+                Collection<Movie> watchedMovies) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.fullName = firstName + " " + lastName;
         this.email = email;
         this.password = password;
-        this.created = LocalDate.now();
         this.country = country;
+        this.watchedMovies = watchedMovies;
+        this.dateCreated = LocalDate.now();
     }
 
     public User(String firstName,
                 String lastName,
                 String email,
                 String password,
-                String country) {
+                String country,
+                Collection<Movie> watchedMovies) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.fullName = firstName + " " + lastName;
         this.email = email;
         this.password = password;
-        this.created = LocalDate.now();
         this.country = country;
+        this.watchedMovies = watchedMovies;
+        this.dateCreated = LocalDate.now();
     }
 
 }
--- src/main/java/com/m2i/showtime/yak/user/UserConfig.java
+++ src/main/java/com/m2i/showtime/yak/user/UserConfig.java
@@ -1,15 +1,20 @@
 package com.m2i.showtime.yak.user;
 
+import com.m2i.showtime.yak.movie.Movie;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+
+import static com.fasterxml.jackson.databind.type.LogicalType.Collection;
 
 @Configuration
 public class UserConfig {
 
-    @Bean
+/*    @Bean
     CommandLineRunner commandLineRunner(UserRepository userRepository) {
         return args -> {
             User achot = new User(
@@ -17,20 +22,13 @@ public class UserConfig {
                     "Barseghyan",
                     "achot.barseghyan@gmail.com",
                     "Password123",
-                    "France"
-            );
-
-            User kevin = new User(
-                    "Kevin",
-                    "Pognon",
-                    "kevin.pognon@gmail.com",
-                    "Password123",
-                    "France"
+                    "France",
+                    Collection("The batman"));
             );
 
             userRepository.saveAll(
-                    List.of(achot, kevin)
+                    List.of(achot)
             );
         };
-    }
+    }*/
 }
