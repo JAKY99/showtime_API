8ae689c857043f322e8554264a08675f5d98a097
unknown <kev972@gmail.com>
2022-07-09 23:53:54 +0200

    maj devops

--- docker/ubuntu/ubuntu.sh
+++ docker/ubuntu/ubuntu.sh
@@ -3,5 +3,5 @@ kill $(ps aux | grep "java -jar target/yak-0.0.1-SNAPSHOT.jar" | grep -v 'grep' 
 # cat /root/project/src/main/resources/application.properties
 # java -jar target/yak-0.0.1-SNAPSHOT.jar
 chmod -R 777 /root/project
-chmod +x mvnw && dos2unix mvnw  && ./mvnw clean install spring-boot:repackage && ./mvnw package 
+chmod +x mvnw && dos2unix mvnw  && ./mvnw clean install spring-boot:repackage
 ./mvnw spring-boot:run
