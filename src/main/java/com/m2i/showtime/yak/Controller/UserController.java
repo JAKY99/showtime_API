d4b9e621ccf9d0c89693006fb1d5a4f5cc6f1f3c
Achot BARSEGHYAN <achot.barseghyan@gmail.com>
2022-07-14 10:32:46 +0200

    feat: project structured by layer

---
+++ src/main/java/com/m2i/showtime/yak/Controller/UserController.java
@@ -0,0 +1,43 @@
+package com.m2i.showtime.yak.Controller;
+
+import com.m2i.showtime.yak.Entity.User;
+import com.m2i.showtime.yak.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getUsers(){
+        return userService.getUsers();
+    }
+
+    @PostMapping
+    public void insertNewUser(@RequestBody User user){
+        userService.addNewUser(user);
+    }
+
+    @PutMapping(path = "{userId}")
+    public void updateUser(
+            @PathVariable("userId") Long userId,
+            @RequestBody User modifiedUser
+        ) {
+        userService.updateUser(userId, modifiedUser);
+    }
+
+    @DeleteMapping(path = "{userId}")
+    public void deleteUser(@PathVariable("userId") Long userId){
+        userService.deleteUser(userId);
+    }
+}
--- src/main/java/com/m2i/showtime/yak/Repository/CategoryRepository.java
+++ src/main/java/com/m2i/showtime/yak/Repository/CategoryRepository.java
@@ -1,5 +1,6 @@
-package com.m2i.showtime.yak.category;
+package com.m2i.showtime.yak.Repository;
 
+import com.m2i.showtime.yak.Entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
