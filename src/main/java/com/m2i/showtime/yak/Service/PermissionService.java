24b47fb1d208d55f289cd1a8b29a89994a939372
Achot BARSEGHYAN <achot.barseghyan@gmail.com>
2022-10-28 06:55:04 +0200

    WIP TEMP

--- src/main/java/com/m2i/showtime/yak/Service/PermissionService.java
+++ src/main/java/com/m2i/showtime/yak/Service/PermissionService.java
@@ -2,9 +2,18 @@ package com.m2i.showtime.yak.Service;
 
 import com.m2i.showtime.yak.Dto.Search.PageListResultDto;
 import com.m2i.showtime.yak.Dto.Search.SearchParamsDto;
+import com.m2i.showtime.yak.Entity.Permission;
 import com.m2i.showtime.yak.Repository.PermissionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+import java.util.Optional;
 
 @Service
 public class PermissionService {
@@ -18,7 +27,45 @@ public class PermissionService {
 
     public PageListResultDto getPermissions(SearchParamsDto searchParamsDto) {
 
-        return permissionRepository.getPermissionsList(searchParamsDto.getLimitRow(), searchParamsDto.getFirst(),
-                                                       searchParamsDto.getSort(), searchParamsDto.getFilters());
+        Page<Permission> page;
+        Sort.Direction sortDirection;
+
+        if (searchParamsDto.getSort()
+                           .getSortField() == null) {
+
+            page = permissionRepository.findAll(
+                    PageRequest.of(searchParamsDto.getPageNumber(), searchParamsDto.getLimitRow(),
+                                   Sort.by(Sort.Direction.ASC, "displayName")));
+        } else {
+            if (searchParamsDto.getSort()
+                               .getSortOrder() == 1) {
+                sortDirection = Sort.Direction.ASC;
+            } else {
+                sortDirection = Sort.Direction.DESC;
+            }
+            page = permissionRepository.findAll(
+                    PageRequest.of(searchParamsDto.getPageNumber(), searchParamsDto.getLimitRow(),
+                                   Sort.by(sortDirection, searchParamsDto.getSort()
+                                                                         .getSortField())));
+        }
+
+        if (page.isEmpty()) {
+            throw new IllegalStateException("page was not found");
+        }
+
+        return new PageListResultDto(page.toList(), page.getTotalElements());
     }
+
+    public long addPermission(Permission permission) {
+        try {
+
+            return permissionRepository.save(permission)
+                                       .getId();
+        } catch (DataIntegrityViolationException e) {
+            throw new IllegalStateException("Permission already exists.");
+        }
+
+    }
+
+    ;
 }
