0fd76bacc18e25d861b2fde9721d13e8419032a7
unknown <kev972@gmail.com>
2022-11-02 00:22:24 +0100

    maj migration

--- make-migration.sh
+++
@@ -1,7 +0,0 @@
-##bin/sh
-#MIGRATION_LABEL = "to-be-changed"
-#DATE_WITH_TIME = "$(date +"%Y-%m-%d-%H:%M:%S")"
-#
-#	./mvnw liquibase:diff -DdiffChangeLogFile=src/main/resources/db/changelog/changes/${DATE_WITH_TIME}-${MIGRATION_LABEL}.yaml
-##	@echo "  - include:" >> src/main/resources/db/changelog/db.changelog-master.yaml
-##	@echo "      file: classpath*:db/changelog/changes/$(DATE_WITH_TIME)-$(MIGRATION_LABEL).yaml" >> src/main/resources/db/changelog/db.changelog-master.yaml
