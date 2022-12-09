1f63d2264fc73cc1ee3ae3e5958114bb6d8862b4
kevP-Sirius <kev972@gmail.com>
2022-12-09 03:32:15 +0100

    maj config kafka

--- src/main/java/com/m2i/showtime/yak/Configuration/KafkaProducerConfig.java
+++ src/main/java/com/m2i/showtime/yak/Configuration/KafkaProducerConfig.java
@@ -29,6 +29,18 @@ public class KafkaProducerConfig {
         configProps.put(
                 ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
                 StringSerializer.class);
+        configProps.put(ProducerConfig.MAX_REQUEST_SIZE_CONFIG, "209715200");
+        configProps.put(ProducerConfig.BATCH_SIZE_CONFIG, "197152");
+        configProps.put(ProducerConfig.BUFFER_MEMORY_CONFIG, "209715200");
+        configProps.put(ProducerConfig.LINGER_MS_CONFIG, "0");
+        configProps.put(ProducerConfig.RETRIES_CONFIG, "3");
+        configProps.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, "30000");
+        configProps.put(ProducerConfig.MAX_BLOCK_MS_CONFIG, "30000");
+        configProps.put(ProducerConfig.ACKS_CONFIG, "all");
+        configProps.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, "1000");
+        configProps.put(ProducerConfig.RECONNECT_BACKOFF_MS_CONFIG, "1000");
+        configProps.put(ProducerConfig.RECONNECT_BACKOFF_MAX_MS_CONFIG, "1000");
+        configProps.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
