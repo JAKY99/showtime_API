cf14f14c0103369765ef3213147b7db38f6a1cdd
kevP-Sirius <kev972@gmail.com>
2022-12-09 22:16:27 +0100

    maj kibana+elasticsearch secure pack

--- src/main/java/com/m2i/showtime/yak/Dto/RunInsertFromIdDto.java
+++ src/main/java/com/m2i/showtime/yak/Dto/RunInsertFromIdDto.java
@@ -12,20 +12,27 @@ public class RunInsertFromIdDto {
     String dateStrFormatted;
     String elasticbaseUrl;
     String apiKey;
+    String elasticUsername;
+    String elasticPassword;
 
-    public RunInsertFromIdDto(int id, int index, String dateStrFormatted, String elasticbaseUrl, String apikey,File toFile){
+    public RunInsertFromIdDto(int id, int index, String dateStrFormatted, String elasticbaseUrl, String apikey,File toFile, String elasticUsername, String elasticPassword) {
         this.elementId = id;
         this.indexUrlToUse = index;
         this.dateStrFormatted = dateStrFormatted;
         this.elasticbaseUrl = elasticbaseUrl;
         this.apiKey = apikey;
         this.toFile = toFile;
+        this.elasticUsername = elasticUsername;
+        this.elasticPassword = elasticPassword;
     }
-    public RunInsertFromIdDto(int index, String dateStrFormatted, String elasticbaseUrl, String apikey,File toFile){
+    public RunInsertFromIdDto(int index, String dateStrFormatted, String elasticbaseUrl, String apikey,File toFile, String elasticUsername, String elasticPassword) {
         this.indexUrlToUse = index;
         this.dateStrFormatted = dateStrFormatted;
         this.elasticbaseUrl = elasticbaseUrl;
         this.apiKey = apikey;
         this.toFile = toFile;
+        this.elasticUsername = elasticUsername;
+        this.elasticPassword = elasticPassword;
+
     }
 }
--- src/main/java/com/m2i/showtime/yak/Service/CustomThreadService.java
+++ src/main/java/com/m2i/showtime/yak/Service/CustomThreadService.java
@@ -9,11 +9,13 @@ package com.m2i.showtime.yak.Service;
         import org.json.JSONObject;
 
         import java.io.*;
+        import java.net.Authenticator;
         import java.net.URI;
         import java.net.URISyntaxException;
         import java.net.http.HttpClient;
         import java.net.http.HttpRequest;
         import java.net.http.HttpResponse;
+        import java.util.Base64;
 
         import static org.apache.kafka.common.utils.Utils.readFileAsString;
 
@@ -44,7 +46,7 @@ public class CustomThreadService extends Thread{
                     runInsertBulk(currentBulkDto.getElasticbaseUrl(),currentBulkDto.getCurrentUrl(),currentBulkDto.getPage(),currentBulkDto.getUrlMovieUserElastic());
                     break;
                 case "readJsonFile":
-                    readJsonFile(RunInsertFromIdDto.getToFile(),RunInsertFromIdDto.getIndexUrlToUse(),RunInsertFromIdDto.getDateStrFormatted(),RunInsertFromIdDto.getElasticbaseUrl());
+                    readJsonFile(RunInsertFromIdDto.getToFile(),RunInsertFromIdDto.getIndexUrlToUse(),RunInsertFromIdDto.getDateStrFormatted(),RunInsertFromIdDto.getElasticbaseUrl(),RunInsertFromIdDto.getElasticUsername(),RunInsertFromIdDto.getElasticPassword());
                     break;
             }
         } catch (Exception e) {
@@ -87,20 +89,25 @@ public class CustomThreadService extends Thread{
                 }
             }
    }
-    private void readJsonFile(File toFile,int index,String dateStrFormatted,String elasticbaseUrl)  {
+    private void readJsonFile(File toFile,int index,String dateStrFormatted,String elasticbaseUrl,String elasticUsername,String elasticPassword)  {
         try {
             BufferedReader br = new BufferedReader(new FileReader(toFile));
             String[] elementType = {"movie","tv","person"};
             String line = null;
             HttpClient client = HttpClient.newHttpClient();
             StringBuilder builk_build_string = new StringBuilder();
+            LOGGER.print("Bulk start - genererating bulk from file : " + toFile.getName());
             while ((line = br.readLine()) != null) {
                 JSONObject obj = new JSONObject(line);
                 builk_build_string.append("{ \"index\":{ \"_index\": \""+ elementType[index] + "s_"+ dateStrFormatted + "\" } }\n");
                 builk_build_string.append( obj + "\n");
             }
-            LOGGER.print("Bulk terminé");
-            this.RunInsertFromId(builk_build_string,index,dateStrFormatted,elasticbaseUrl,client);
+            br.close();
+            toFile.delete();
+            LOGGER.print("Bulk genererating done - Deleting file : " + toFile.getName());
+            toFile.delete();
+            LOGGER.print("sending bulk : " + elementType[index] + " - to elastic " );
+            this.RunInsertFromId(builk_build_string,index,dateStrFormatted,elasticbaseUrl,client,elasticUsername,elasticPassword);
 
 
         } catch (FileNotFoundException e) {
@@ -113,22 +120,25 @@ public class CustomThreadService extends Thread{
             throw new RuntimeException(e);
         }
     }
-    public void RunInsertFromId(StringBuilder row , int indexUrlToUse, String dateStrFormatted, String elasticbaseUrl, HttpClient client) throws URISyntaxException, IOException, InterruptedException {
+
+    private static final String getBasicAuthenticationHeader(String username, String password) {
+        String valueToEncode = username + ":" + password;
+        return "Basic " + Base64.getEncoder().encodeToString(valueToEncode.getBytes());
+    }
+    public void RunInsertFromId(StringBuilder row , int indexUrlToUse, String dateStrFormatted, String elasticbaseUrl, HttpClient client,String elasticUsername,String elasticPassword) throws URISyntaxException, IOException, InterruptedException {
         String[] elementType = {"movie","tv","person"};
 
 
-        LOGGER.print("Insertion " + elementType[indexUrlToUse]+ " row : " + row + " - Début");
+        LOGGER.print("Insertion " + elementType[indexUrlToUse]+ " bulk - Start");
             try {
-
                    HttpRequest elasticInsert = HttpRequest.newBuilder()
                            .uri(new URI(elasticbaseUrl + "/" + elementType[indexUrlToUse] + "s_" + dateStrFormatted + "/_bulk"))
                            .setHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                           .setHeader(HttpHeaders.AUTHORIZATION,getBasicAuthenticationHeader(elasticUsername, elasticPassword))
                            .PUT(HttpRequest.BodyPublishers.ofString(row.toString()))
                            .build();
                    HttpResponse elasticResponse = client.send(elasticInsert, HttpResponse.BodyHandlers.ofString());
-
-                LOGGER.print("Insertion " + elementType[indexUrlToUse]+ " row : - Fin");
-
+                LOGGER.print("Insertion " + elementType[indexUrlToUse]+ " bulk - Over - Status code : " + elasticResponse.statusCode());
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
