ae2a2c574d8849ef3494b3d8a99df73b72c3b6c9
kevP-Sirius <kev972@gmail.com>
2023-01-15 19:46:11 +0100

    maj sonarqube + healthcheck

--- src/main/java/com/m2i/showtime/yak/Controller/HealthCheckController.java
+++ src/main/java/com/m2i/showtime/yak/Controller/HealthCheckController.java
@@ -4,12 +4,12 @@ import com.m2i.showtime.yak.Dto.HealthCheckStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 @RequestMapping("api/v1/health")
 public class HealthCheckController {
+
     @GetMapping("/check")
     public HealthCheckStatus healthCheck() {
-        return new HealthCheckStatus();
+        return  new HealthCheckStatus("OK");
     }
 }
--- src/main/java/com/m2i/showtime/yak/Dto/HealthCheckStatus.java
+++ src/main/java/com/m2i/showtime/yak/Dto/HealthCheckStatus.java
@@ -1,5 +1,5 @@
 package com.m2i.showtime.yak.Dto;
 
-public class HealthCheckStatus {
-    String status = "OK";
+public record HealthCheckStatus (String status) {
+
 }
