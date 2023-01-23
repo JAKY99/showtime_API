1a188c012c55eb0f8eabb23ad16ef6a1efba0f69
kevP-Sirius <kev972@gmail.com>
2023-01-23 03:37:19 +0100

    correction code sonarqube jacoco

--- src/main/java/com/m2i/showtime/yak/Dto/RedisPictureDto.java
+++ src/main/java/com/m2i/showtime/yak/Dto/RedisPictureDto.java
@@ -1,5 +1,12 @@
 package com.m2i.showtime.yak.Dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
 public class RedisPictureDto {
     String poster_path;
     String backdrop_path;
--- src/main/java/com/m2i/showtime/yak/Dto/RunInsertRedisCacheDto.java
+++ src/main/java/com/m2i/showtime/yak/Dto/RunInsertRedisCacheDto.java
@@ -2,10 +2,12 @@ package com.m2i.showtime.yak.Dto;
 
 import com.m2i.showtime.yak.Configuration.RedisConfig;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class RunInsertRedisCacheDto {
     private  RedisConfig redisConfig;
     private  String urlApi;
