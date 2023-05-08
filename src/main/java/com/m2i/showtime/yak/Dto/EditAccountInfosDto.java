1216c078d146556eb90db9f15e669be0658475e3
Achot BARSEGHYAN <achot.barseghyan@gmail.com>
2023-05-08 14:09:06 +0200

    feature/editAccountInfos

---
+++ src/main/java/com/m2i/showtime/yak/Dto/EditAccountInfosDto.java
@@ -0,0 +1,11 @@
+package com.m2i.showtime.yak.Dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EditAccountInfosDto {
+    public String firstName;
+    public String lastName;
+}
